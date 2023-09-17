161e3007f8396ca1b3b0bf4cf619c6e7982ea552
Roshan-Rajurkar <roshangit50@gmail.com>
2023-09-17 22:07:12 +0530

    first backend commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const router = require("./routes/book-routes");
+const cors = require("cors");
+const app = express();
+const dotenv = require('dotenv')
+
+dotenv.config();
+
+const PORT = process.env.PORT_NUMBER
+const DB_URI = process.env.DATABASE_URL
+
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+app.use("/books", router);
+
+mongoose
+  .connect(
+    DB_URI
+  )
+  .then(() => console.log("Connected To Database"))
+  .then(() => {
+    app.listen(PORT, () => console.log("Server running"));
+  })
+  .catch((err) => console.log(err));
